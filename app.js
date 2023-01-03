8ec51c3788d4591886121696180b091d5a8bf5a9
Jiteshkumar yadav <9766jitesh@gmail.com>
2023-01-03 23:25:56 +0530

    completed

--- app.js
+++ app.js
@@ -1,24 +1,33 @@
-const row=document.getElementById("row");
+const mendiv=document.getElementById("mendiv");
 const column=document.getElementById("column");
+const row=document.getElementById("row");
 const men=document.getElementById("men");
-const sec1=document.getElementById("section1");
+const women=document.getElementById("women");
+const womendiv=document.getElementById("womendiv");
 
-function mouseOver(){
-    row.style.display="block";
-    row.style.position="absolute";
+
+function menMouseOver(){
+    mendiv.style.display="block";
     men.style.cursor="pointer";
-    console.log(column);
-    column.style.position="relative";
-    
-    
-    
+    row.style.position="absolute";
+    column.style.position="relative";  
 }
-function mouseOut(){
-    row.style.display="none";
+function menMouseOut(){
+    mendiv.style.display="none";
     men.style.cursor="arrow";
-    console.log(sec1.style.position);
-   
-
 }
-men.addEventListener('mouseover',mouseOver);
-men.addEventListener('mouseout',mouseOut);
+men.addEventListener('mouseover',menMouseOver);
+men.addEventListener('mouseout',menMouseOut);
+function womenMouseOver(){
+    womendiv.style.display="block";
+    womendiv.style.border="10px red";
+    women.style.cursor="pointer";
+    row.style.position="absolute";
+    column.style.position="relative";  
+}
+function womenMouseOut(){
+    womendiv.style.display="none";
+    women.style.cursor="arrow";
+}
+women.addEventListener('mouseover',womenMouseOver);
+women.addEventListener('mouseout',womenMouseOut);
